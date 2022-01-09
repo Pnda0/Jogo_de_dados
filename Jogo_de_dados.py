bade96a7ce614bd0cd7d1bc7d1a22c4bf22c49bc
Alan Ribeiro <78491224+Pnda0@users.noreply.github.com>
2022-01-08 21:29:38 -0300

    Add files via upload

---
+++ Jogo_de_dados.py
@@ -0,0 +1,26 @@
+import random
+from tkinter import *
+
+
+class Diceroller(object):
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.label = Label(master, font=('times', 200))
+        button = Button(master, text='Rolar dados', command=self.roll)
+        button.place(x=200, y=0)
+
+    def roll(self):
+        symbol = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+        self.label.config(
+            text=f'{random.choice(symbol)}{random.choice(symbol)}')
+        self.label.pack()
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Jogo de Dados')
+    root.geometry('500x300')
+    Diceroller(root)
+    root.mainloop()
